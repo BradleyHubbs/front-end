9d2f043d86ceda8f3f51f731a0eb39feeff3decc
Daniel B Kim <danielkim@Daniels-MacBook-Pro.local>
2021-03-28 20:25:12 -0400

    adds login page HTML skeleton

--- water-my-plants/src/components/LoginPage.js
+++ water-my-plants/src/components/LoginPage.js
@@ -1,27 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function LoginPage(props) {
     return(
         <div className='login-form'>
             <h1>water my plants</h1>
             <form>
-                <label>
+                <label htmlFor='username'>
                     Username:
-                    <input
-                        name='username'
-                        type='text'
-                    />
                 </label>
+                <input
+                    name='username'
+                    type='text'
+                />
                 <br/>
                 <br/>
-                <label>
+                <label htmlFor='password'>
                     Password:
-                    <input
-                        name='password'
-                        type='text'
-                    />
                 </label>
+                <input
+                    name='password'
+                    type='password'
+                />
+                <br/>
+                <br/>
+                <button id='submitBtn'>Sign In</button>
             </form>
         </div>
     )
-}
+};
